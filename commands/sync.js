b7058dc8b3cecfff7abd2d45cbd5747de0283593
shishi42 <benji.riviere72@hotmail.fr>
2023-07-21 17:57:31 +0200

    update sync

--- commands/sync.js
+++ commands/sync.js
@@ -28,9 +28,27 @@ module.exports = {
     ["1074969062515421224","A"],
     ["1012306136193585193","C"]]
 
+    ids2 =
+    // [["1012306317890814022","A"],
+    // ["1012302626660032623","D"],
+    ["1012302627075280916","D"],
+    ["1012306014361616384","C"],
+    // ["1012302555134570506","B"],
+    // ["1012302627083669645","C"],
+    ["1012302578446520330","A"],
+    ["1012306088059744307","B"],
+    ["1012305995118153738","C"],
+    // ["1012306116304187413","B"],
+    ["1012305976327688192","D"],
+    // ["1086348047103033425","D"],
+    ["1012306070770823199","B"],
+    // ["1012306163825651767","A"],
+    ["1074969062515421224","A"],
+    // ["1012306136193585193","C"]]
+
     captain = "1012657654289661963"
 
-    ids.forEach(id => {
+    ids2.forEach(id => {
       bot.Teams.create({team_id: bot.guilds.cache.get(bot.wc).roles.cache.get(id[0]).id, team_name: bot.guilds.cache.get(bot.wc).roles.cache.get(id[0]).name, team_alias: bot.guilds.cache.get(bot.wc).roles.cache.get(id[0]).name.replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g,'').trim(), team_group: id[1]})
       const Role = bot.guilds.cache.get(bot.wc).roles.cache.find(role => role.id == id[0])
       let team_players = bot.guilds.cache.get(bot.wc).members.cache.filter(member => member.roles.cache.find(role => role == Role)).map(member => member)
