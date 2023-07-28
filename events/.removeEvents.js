a29b18373fb240e6f0ff529e643f419c990708d4
Shishi <37541779+Shishi42@users.noreply.github.com>
2023-07-29 00:46:45 +0200

    fix

--- events/.removeEvents.js
+++ events/.removeEvents.js
@@ -7,8 +7,8 @@ module.exports = {
     // require(`../events/.log.js`).run(bot, `[LAUNCH-DELETE-EVENT-AUTO]`)
 
     bot.channels.cache.get(bot.announcement).messages.fetch().then(messages => messages.forEach(message => {
-      // id = message.content.split("/")[5]
-      id = message.content.split("event=")[1]
+      id = message.content.split("/")[5]
+      // id = message.content.split("event=")[1]
       bot.guilds.cache.get(bot.wc).scheduledEvents.fetch(id).then(event => {
         if(event.isCompleted()){
           message.delete()
