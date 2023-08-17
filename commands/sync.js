583fcdb17ea7ca5ef3c4673d8db1cc9a97985372
shishi42 <benji.riviere72@hotmail.fr>
2023-08-18 01:32:52 +0200

    final sync

--- commands/sync.js
+++ commands/sync.js
@@ -10,7 +10,7 @@ module.exports = {
 
   async run(bot, message, args) {
 
-    ids =
+    group_stage =
     [["1012306317890814022","A"],
     ["1012302626660032623","D"],
     ["1012302627075280916","D"],
@@ -28,25 +28,25 @@ module.exports = {
     ["1074969062515421224","A"],
     ["1012306136193585193","C"]]
 
-    ids2 =
-    [["1012302627075280916","D"],
-    ["1012306014361616384","C"],
-    ["1012302578446520330","A"],
-    ["1012306088059744307","B"],
-    ["1012305995118153738","C"],
-    ["1012305976327688192","D"],
-    ["1012306070770823199","B"],
-    ["1074969062515421224","A"]]
+    knockout_stage =
+    [["1012302627075280916","X"],
+    ["1012306014361616384","X"],
+    ["1012302578446520330","X"],
+    ["1012306088059744307","X"],
+    ["1012305995118153738","X"],
+    ["1012305976327688192","X"],
+    ["1012306070770823199","X"],
+    ["1074969062515421224","X"]]
 
-    ids3 = [["1137051846406578176","A"], ["1137052068155236384","B"]]
+    final_stage = [["1137051846406578176","X"], ["1137052068155236384","X"]]
 
-    dev_ids =
-    [["841348652693585981","A"],
-    ["1064236599195467898","B"]]
+    dev_stage =
+    [["841348652693585981","X"],
+    ["1064236599195467898","X"]]
 
     captain = "1012657654289661963"
 
-    ids3.forEach(id => {
+    group_stage.forEach(id => {
       bot.Teams.create({team_id: bot.guilds.cache.get(bot.wc).roles.cache.get(id[0]).id, team_name: bot.guilds.cache.get(bot.wc).roles.cache.get(id[0]).name, team_alias: bot.guilds.cache.get(bot.wc).roles.cache.get(id[0]).name.replace(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g,'').trim(), team_group: id[1]})
       const Role = bot.guilds.cache.get(bot.wc).roles.cache.find(role => role.id == id[0])
       let team_players = bot.guilds.cache.get(bot.wc).members.cache.filter(member => member.roles.cache.find(role => role == Role)).map(member => member)
